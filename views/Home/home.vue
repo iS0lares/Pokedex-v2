ca2d87a25b778d7da41fd05ccc6ae4ce1063adc4
moisessolares <moisessolaressantos@gmail.com>
2025-03-20 13:28:12 -0300

    Feat: create new archive to store the code of page home

---
+++ views/Home/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mt-16">
+    <div>
+      <v-row>
+        <v-col>
+          <h1 class="title">WELCOME</h1>
+          <h2 class="subtitle">To Pokedex</h2>
+        </v-col>
+        <v-col>
+          <v-img src="/capa_pokedex.png"></v-img>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.title {
+  font-size: 7rem;
+  font-weight: 500;
+  line-height: 1;
+  letter-spacing: -0.015625em;
+}
+
+.subtitle {
+  font-size: 3.75rem;
+  font-weight: 300;
+  line-height: 1;
+  letter-spacing: -0.0083333333em;
+}
+</style>
