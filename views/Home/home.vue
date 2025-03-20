b66f539aed200035be2966ff93a9f87634c747be
moisessolares <moisessolaressantos@gmail.com>
2025-03-20 16:22:17 -0300

    Feat: add content on home page

--- views/Home/home.vue
+++ views/Home/home.vue
@@ -2,24 +2,83 @@
   <div class="mt-16">
     <div>
       <v-row>
-        <v-col>
+        <v-col cols="6">
           <h1 class="title">WELCOME</h1>
           <h2 class="subtitle">To Pokedex</h2>
         </v-col>
-        <v-col>
-          <v-img src="/capa_pokedex.png"></v-img>
+        <v-col cols="6">
+          <v-row>
+            <v-col class="d-flex justify-center align-center" cols="4">
+              <div>
+                <v-img
+                  class="mx-auto py-auto"
+                  :width="200"
+                  :src="imageFirstResponse"
+                />
+              </div>
+            </v-col>
+            <v-col class="d-flex justify-center align-center" cols="4">
+              <div>
+                <v-img
+                  class="mx-auto mt-10"
+                  :width="200"
+                  :src="imageSecondResponse"
+                />
+              </div>
+            </v-col>
+            <v-col navigateTo cols="4">
+              <NuxtLink to="/pokedex">
+                <v-img
+                  class="ml-auto mt-3"
+                  :width="200"
+                  src="/capa_pokedex.png"
+                />
+              </NuxtLink>
+            </v-col>
+          </v-row>
+          <v-col cols="auto"> </v-col>
         </v-col>
       </v-row>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from "axios";
+
+const maxIdPokemon = 300;
+const firstSortedIdPokemon = Math.floor(Math.random() * maxIdPokemon);
+const secondSortedIdPokemon = Math.floor(Math.random() * maxIdPokemon);
+const imageFirstResponse = ref("");
+const imageSecondResponse = ref("");
+
+const requestPokemonId = async () => {
+  try {
+    const responseFirst = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${firstSortedIdPokemon}/`
+    );
+    const responseSecond = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${secondSortedIdPokemon}/`
+    );
+    imageFirstResponse.value =
+      responseFirst.data.sprites.versions["generation-v"][
+        "black-white"
+      ].animated.front_default;
+    imageSecondResponse.value =
+      responseSecond.data.sprites.versions["generation-v"][
+        "black-white"
+      ].animated.front_default;
+  } catch (error) {
+    console.log(error);
+  }
+};
+requestPokemonId();
+</script>
 
 <style scoped>
 .title {
   font-size: 7rem;
-  font-weight: 500;
+  font-weight: 600;
   line-height: 1;
   letter-spacing: -0.015625em;
 }
